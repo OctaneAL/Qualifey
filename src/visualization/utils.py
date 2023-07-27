44544ca7b2dae9e0e9c661aa8fa75ef302e3b7ba
OctaneAL <dimadanik2@gmail.com>
2023-07-27 20:45:05 +0300

    a little bit progress in visualization graph

--- src/visualization/utils.py
+++ src/visualization/utils.py
@@ -1,6 +1,7 @@
 import os, json
 import pandas as pd
 from d3blocks import D3Blocks
+from math import floor
 
 input_path = os.path.join(os.getcwd(), 'visualization', 'data')
 output_path = os.path.join(os.getcwd(), 'visualization', 'graphs')
@@ -8,22 +9,51 @@ output_path = os.path.join(os.getcwd(), 'visualization', 'graphs')
 def visualize_graph(input_name: str = 'global_graph.json', output_name: str = 'd3graph.html'):
     graph = read_json(os.path.join(input_path, input_name))
     
+    k = 20
+    temp = []
+
     source_list = []
     target_list = []
     weight_list = []
+    count1 = 0
+    count2 = 0
     for source in graph:
+        t = 0
+        count1 += 1
         for target in graph[source]:
-            source_list.append(source)
-            target_list.append(target)
-            weight_list.append(graph[source][target])
+            if graph[source][target] > 50:
+                count2 += 1
+                source_list.append(source)
+                target_list.append(target)
+                weight_list.append(graph[source][target] / k)
+                t += graph[source][target] / k
+            if count2 == 10:
+                break
+        if t != 0:
+            temp.append(t)
+        if count1 == 10:
+            break
+    temp.pop()
         
     data = {
         'source': source_list,
         'target': target_list,
         'weight': weight_list,
     }
-    
+    x = min(data['weight'])
+    step = (max(data['weight']) - x) / 10
+    max_size = 1000
+    # sizes = [int(floor((weight - x) / step + 1)) for weight in temp]
+    sizes = [(weight - x) / (max_size - x) * max_size for weight in temp]
+
+    data2 = {
+        'source': source_list[k:],
+        'target': target_list[k:],
+        'weight': weight_list[k:],
+    }
+
     df = pd.DataFrame(data)
+    df2 = pd.DataFrame(data2)
 
     # d3 = D3Blocks()
 
@@ -39,8 +69,17 @@ def visualize_graph(input_name: str = 'global_graph.json', output_name: str = 'd
     # df = d3.import_example('energy') # 'bigbang', 'stormofswords'
     #
     # Create network using default
-    d3.d3graph(df, filepath='d3graph.html', charge = 200000)
-    d3.D3graph.set_edge_properties(minmax_distance=[1000, 10000])
+
+    print(sizes)
+    print()
+    sizes.append(5)
+    print(len(sizes))
+    d3.d3graph(df, filepath='d3graph.html', showfig=False, size = 1)
+    print(d3.D3graph.adjmat.shape[0])
+    d3.D3graph.set_node_properties(minmax = [0, 5000], size = 5)
+    # d3.D3graph.set_node_properties['size'] = sizes
+    # d3.d3graph(df2, filepath='d3graph.html', charge = 400, showfig=False, size = 10)
+    d3.D3graph.set_edge_properties(minmax_distance=[2, 1000])
     d3.D3graph.show()
     return
     # Change scaler
