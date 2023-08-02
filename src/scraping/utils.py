9b0343c50e6c12b9fbfb35bd94e7c44a3d7e34e3
OctaneAL <dimadanik2@gmail.com>
2023-08-02 10:46:22 +0300

    correct creating and visualizing graph

--- src/scraping/utils.py
+++ src/scraping/utils.py
@@ -31,7 +31,6 @@ def skills_connections_to_graph(input_name: str = 'skills_connections.json', out
     subskills_name = 'subskills.json'
     subskills = read_json(os.path.join(current_path, subskills_name))
 
-    graph = {} # name: {name1: weight1, name2: weight2, ...}
     node_sizes = {}
     edge_weights = {}
     # memoization???
@@ -41,64 +40,17 @@ def skills_connections_to_graph(input_name: str = 'skills_connections.json', out
         items[id]['links'] = clear_items(items[id]['links'])
 
         for i, source in enumerate(items[id]['links']):
-            # if source in subskills:
-            #     source_main_skill = subskills[source]
-            #     add_weight(source_main_skill, source, weight)
-            #     add_weight(source, source_main_skill, weight)
-            #     source = source_main_skill
-
             add_size(source, weight)
             if source in subskills:
-                # add_weight(source, subskills[source], weight)
-                # add_weight(subskills[source], source, weight)
                 source = subskills[source]
-                # add_size(source, weight)
 
             for target in items[id]['links'][i+1:]:
                 if target in subskills:
-                    # add_weight(target, subskills[target], weight)
-                    # add_weight(subskills[target], target, weight)
                     target = subskills[target]
-                    # add_size(target, weight)
                 
                 add_weight(source, target, weight)
                 add_weight(target, source, weight)
 
-
-                # t_source = source
-                # if source in subskills:
-                #     source_main_skill = subskills[source]
-                #     add_weight(source_main_skill, source, weight)
-                #     add_weight(source, source_main_skill, weight)
-                #     t_source = source_main_skill
-                # if target in subskills:
-                #     target_main_skill = subskills[target]
-                #     add_weight(target_main_skill, target, weight)
-                #     add_weight(target, target_main_skill, weight)
-                #     target = subskills[target]
-                
-                # add_weight(t_source, target, weight)
-                # add_weight(target, t_source, weight)
-
-
-
-                # if not target in graph[source]:
-                #     graph[source][target] = weight
-                # else:
-                #     graph[source][target] += weight
-                
-
-                # if target in subskills:
-                #     target = subskills[target]
-                # if not target in graph:
-                #     graph[target] = {}
-                
-                # if not source in graph[target]:
-                #     graph[target][source] = weight
-                # else:
-                #     graph[target][source] += weight
-    
-    # DECREASE EDGES SIZES BY 2 TIMES
     for source in edge_weights:
         for target in edge_weights[source]:
             edge_weights[source][target] /= 2
@@ -109,12 +61,6 @@ def skills_connections_to_graph(input_name: str = 'skills_connections.json', out
     }
 
     write_json(graph, os.path.join(visualization_path, 'temp.json'))
-
-    # for source in graph.keys():
-    #     for target in graph[source].keys():
-    #         graph[source][target] /= 2
-    
-    # write_json(graph, os.path.join(visualization_path, output_name))
 
 def write_json(_json, path: str):
     with open(path, 'w', encoding='utf-8') as f:
--- src/visualization/utils.py
+++ src/visualization/utils.py
@@ -8,39 +8,42 @@ output_path = os.path.join(os.getcwd(), 'templates')
 
 def visualize_graph(input_name: str = 'global_graph.json', output_name: str = 'd3graph.html'):
     graph = read_json(os.path.join(input_path, input_name))
-    
-    k = 20
-    temp = []
 
-    source_list = []
-    target_list = []
-    weight_list = []
-    count1 = 0
-    count2 = 0
-    sizes_ = {}
-    for source in graph:
-        t = 0
-        count1 += 1
-        for target in graph[source]:
-            if not target in sizes_:
-                sizes_[target] = 0
-            if not source in sizes_:
-                sizes_[source] = 0
-            sizes_[target] += graph[source][target]
-            sizes_[source] += graph[source][target]
-            if graph[source][target] > 200:
-                count2 += 1
-                source_list.append(source)
-                target_list.append(target)
-                weight_list.append(graph[source][target] / k)
-                t += graph[source][target] / k
-            if count2 == 50:
-                break
-        if t != 0:
-            temp.append(t)
-        if count1 == 50:
-            break
-    temp.pop()
+    node_sizes = graph['node_sizes']
+    edge_weights = graph['edge_weights']
+    
+    # k = 20
+    # temp = []
+
+    # source_list = []
+    # target_list = []
+    # weight_list = []
+    # count1 = 0
+    # count2 = 0
+    # sizes_ = {}
+    # for source in graph:
+    #     t = 0
+    #     count1 += 1
+    #     for target in graph[source]:
+    #         if not target in sizes_:
+    #             sizes_[target] = 0
+    #         if not source in sizes_:
+    #             sizes_[source] = 0
+    #         sizes_[target] += graph[source][target]
+    #         sizes_[source] += graph[source][target]
+    #         if graph[source][target] > 200:
+    #             count2 += 1
+    #             source_list.append(source)
+    #             target_list.append(target)
+    #             weight_list.append(graph[source][target] / k)
+    #             t += graph[source][target] / k
+    #         if count2 == 50:
+    #             break
+    #     if t != 0:
+    #         temp.append(t)
+    #     if count1 == 50:
+    #         break
+    # temp.pop()
     # for i in range(len(source_list)):
     #     if not source_list[i] in sizes_:
     #         sizes_[source_list[i]] = 0
@@ -48,7 +51,19 @@ def visualize_graph(input_name: str = 'global_graph.json', output_name: str = 'd
     #         sizes_[target_list[i]] = 0
     #     sizes_[source_list[i]] += weight_list[i]
     #     sizes_[target_list[i]] += weight_list[i]
-        
+
+    source_list = []
+    target_list = []
+    weight_list = []
+    
+    for source in edge_weights:
+        for target in edge_weights[source]:
+            if edge_weights[source][target] < 150: # Коефіцієнт
+                continue
+            source_list.append(source)
+            target_list.append(target)
+            weight_list.append(edge_weights[source][target] / 20) # Коефіцієнт
+
     data = {
         'source': source_list,
         'target': target_list,
@@ -64,9 +79,9 @@ def visualize_graph(input_name: str = 'global_graph.json', output_name: str = 'd
     # d3.D3graph.set_node_properties(minmax = [0, 5000])
     d3.D3graph.set_edge_properties(minmax_distance=[200, 2000], minmax=[0.5, 10])
     # print(d3.D3graph.adjmat.shape[0])
-    for i in d3.D3graph.node_properties:
+    for i in node_sizes:
         if i in d3.D3graph.node_properties:
-            d3.D3graph.node_properties[i]['size'] = sizes_[i] / 2000
+            d3.D3graph.node_properties[i]['size'] = node_sizes[i] / 200
             d3.D3graph.node_properties[i]['fontsize'] = max(d3.D3graph.node_properties[i]['size'] * 0.5, 4)
             d3.D3graph.node_properties[i]['fontcolor'] = '#16EE30'
 
@@ -74,11 +89,11 @@ def visualize_graph(input_name: str = 'global_graph.json', output_name: str = 'd
         if i in d3.D3graph.edge_properties:
             d3.D3graph.edge_properties[i]['weight_scaled'] /= 2
 
-    # print(d3.D3graph.node_properties)
+    print(d3.D3graph.node_properties)
     # print(d3.D3graph.edge_properties)
     # d3.D3graph.set_node_properties['size'] = sizes
     # d3.d3graph(df2, filepath='d3graph.html', charge = 400, showfig=False, size = 10)
-    d3.D3graph.show(filepath=os.path.join(output_path, output_name), showfig = False)
+    d3.D3graph.show(filepath=os.path.join(output_path, output_name), showfig = True)
     
     divide_html_into_blocks(os.path.join(output_path, output_name))
 
