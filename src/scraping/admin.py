c3004ee25f38b5a0579b31b01db426f99a20804c
OctaneAL <dimadanik2@gmail.com>
2023-09-03 08:19:08 +0300

    Create Models

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vacancy, Vacancy_skill, Graph, Skill
 
 # Register your models here.
+admin.site.register(Vacancy)
+admin.site.register(Vacancy_skill)
+admin.site.register(Graph)
+admin.site.register(Skill)
--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Vacancy(models.Model):
+    job_id = models.BigIntegerField()
+    company = models.CharField(max_length=128)
+    salary = models.BigIntegerField()
+    country = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return f'Graph #{self.job_id}'
+
+class Graph(models.Model):
+    keyword = models.CharField(max_length=64)
+    data = models.JSONField()
+
+    def __str__(self):
+        return self.keyword.capitalize()
+    
+class Skill(models.Model):
+    skill = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.skill.capitalize()
+    
+class Vacancy_skill(models.Model):
+    vacancy = models.ForeignKey('Vacancy', on_delete=models.CASCADE)
+    skill = models.ForeignKey('Skill', on_delete=models.CASCADE)
+
+class Skill_phrase(models.Model):
+    skill = models.ForeignKey('Skill', on_delete=models.CASCADE)
+    phrase = models.CharField(max_length=128)
